f33a5f417ea3364981e0e47a35f017c453cec51d
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-11-22 22:32:53 -0300

    basicao

---
+++ sintaxe-variaveis-fluxo/src/TesteVariaveis.java
@@ -0,0 +1,23 @@
+
+public class TesteVariaveis {
+	public static void main(String[] args) {
+		System.out.println();
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		idade = 30 * 20;
+		
+		System.out.println(idade);
+		
+		idade = 30;
+		
+		System.out.println("A idade é: " + idade);
+	}
+}
